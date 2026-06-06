2b96f2388f19460590900ea9546d5a3ef25d13c5
Davis Reis <davisreis1@gmail.com>
2026-06-05 21:37:55 -0600

    made stdin for testing

--- exclusiveTimeOfFunctions/exclusiveTimeOfFunctions.py
+++ exclusiveTimeOfFunctions/exclusiveTimeOfFunctions.py
@@ -1,7 +1,10 @@
+import sys
+from typing import List
+
 class solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         running = []
-        currTimes = [] * n
+        currTimes = [0] * n
         epoc = 0
         for string in logs:
             # split = [id, action, epoc]
@@ -9,14 +12,24 @@ class solution:
             funcId = int(split[0])
             timestamp = int(split[2])
             # adding to stack
-                if split[1] == "start":
-                    # stack is not empty
-                    if len(running) != 0:
-                        currTimes[funcId] += timestamp - epoc
-                    running.append(funcId)
-                    epoc = timestamp
-                else:
-                    currTimes[funcId] += timestamp - epoc + 1
-                    running.pop()
-                    epoc = timestamp
+            if split[1] == "start":
+                # stack is not empty
+                if len(running) != 0:
+                    currTimes[funcId] += timestamp - epoc
+                running.append(funcId)
+                epoc = timestamp
+            else:
+                currTimes[funcId] += timestamp - epoc + 1
+                running.pop()
+                epoc = timestamp
         return currTimes
+
+n = int(input())
+log = []
+for line in sys.stdin:
+    clean = line.rstrip()
+    log.append(clean)
+
+test = solution()
+print(test.exclusiveTime(n, log))
+
